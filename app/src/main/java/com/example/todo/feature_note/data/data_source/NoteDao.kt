eda8d451b533a246636be062cb70fb692511a0d4
vijanam <vijanam@mobitel.lk>
2025-02-10 16:09:33 +0530

    database is done

---
+++ app/src/main/java/com/example/todo/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,30 @@
+package com.example.todo.feature_note.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.todo.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    fun getNotes() : Flow<List<Note>>
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id : Int) : Note?
+
+    @Query("DELETE FROM note WHERE id = :id")
+    suspend fun deleteNote(id : Int)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+
+}
